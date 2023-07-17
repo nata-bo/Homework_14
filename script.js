5e0762f6d39458f740d63b87eecdf029191f3ed9
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-07-17 20:28:08 +0200

    add code

---
+++ script.js
@@ -0,0 +1,19 @@
+// Реализовать страницу, на которой будет 1 кнопка. 
+//Нажатие на кнопку должно вызывать появление картинки (возмите любую из интернета) на 5 сек,
+// после чего картинка должна пропадать.
+
+const hitMeButton = document.getElementById('hitMe');
+
+
+hitMeButton.addEventListener('click', showImage);
+
+function showImage() {
+    const image = document.getElementById("img02");
+    image.style.display = "block";  
+
+    setTimeout(function () {
+        image.style.display = "none";  
+    }, 5000);
+    hitMeButton.append(image);
+
+}
